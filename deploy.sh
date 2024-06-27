603c232aef8d090a8f6fa2c33d672f2ef9c21754
Mark <markeasting@gmail.com>
2024-06-27 15:39:20 +0200

    Adjusted deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 npm run build
 
-git subtree push --prefix dist origin gh-pages
+# git subtree push --prefix dist origin gh-pages
+git push origin :gh-pages && git subtree push --prefix dist origin gh-pages
